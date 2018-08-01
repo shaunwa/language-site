80194fe6f7f545ca892f33a18e3b9b76ff8c164f
shaunwa <shaun@trove.com>
2018-08-01 16:34:46 -0400

    Writing about-page copy

---
+++ src/packages/about/copy.js
@@ -0,0 +1,25 @@
+export const whatIsThisSiteAboutText = `
+  Este sitio y los articulos que contiene son el fruto de muchísimas conversaciones
+  que he tenido con mis amigos y estudiantes hispanohablantes. A través de los años,
+  he aprendido mucho sobre los numerosos problemas que presenta el inglés para los que intentan
+  aprenderlo. Me parece que el problema número uno es que muchas veces - demasiadas - el inglés
+  no se enseña como se habla de verdad. Se enseña una suerte de inglés idealista, muy claro y correcto,
+  que se usa muy poco. No es ninguna sorpresa que la gente tiene problemas en charlar con hablantes nativos -
+  apenas hablan el mismo idioma! Y de eso se trata este sitio, ayudarte hablar y entender el inglés como
+  se habla por los gringos.
+`;
+
+export const whoIsThisGuyWithTheBeardText = `
+  Soy yo. Piensa en mí como el profesor de inglés que siempre deseabas, el profesor que va a hacer de tí un maestro angloparlante.
+  Me encantan los idiomas, y especialmente ayudar a la gente a aprender el inglés. Siempre me ha fascinado
+  los acentos y el efecto que tienen sobre la percepción de la gente.
+  Pocas cosas me dan tanto placer como escuchar a un extranjero decir algo en buen gringlés
+  (eso es lo que llamo el inglés que usamos diariamente aquí en Gringolandia).
+  Voy a enseñarte como hablar como nosotros los gringos.
+`;
+
+export const howDoesItWorkText = `
+  Cada articulo se centra en un tema relacionado con el gringlés - palabras clave, frases raras, pronunciaciones
+  interesantes, etcétera - y te da un montón de ejemplos y audios para practicarlo hasta dominarlo. Además, cada
+  articulo incluye un divertido diálogo descargable, con audio y transcripción, que incorpora el tema.
+`;
